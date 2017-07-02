0568e4b28eedad6537261a09a5e9016620999ba6
pomverte <vietnem@yahoo.fr>
2017-07-02 22:32:17 +0200

    cache TTL

---
+++ src/main/java/fr/pomverte/ExpirationCacheCustomizer.java
@@ -0,0 +1,29 @@
+package fr.pomverte;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
+import org.springframework.cache.CacheManager;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExpirationCacheCustomizer implements CacheManagerCustomizer {
+
+    /** TTL : Time To Live */
+    private long ttl;
+
+    public ExpirationCacheCustomizer(@Value("${spring.redis.ttl}") Long ttl) {
+        this.ttl = (ttl == null) ? -1 : ttl;
+    }
+
+    @Override
+    public void customize(CacheManager cacheManager) {
+        if (cacheManager instanceof RedisCacheManager) {
+            RedisCacheManager redisCache = (RedisCacheManager) cacheManager;
+            redisCache.setDefaultExpiration(ttl);
+            //Map<String, Long> expiresMap = new ConcurrentHashMap<>();
+            //expiresMap.put("users", 10L);
+            //redisCache.setExpires(expiresMap);
+        }
+    }
+}
