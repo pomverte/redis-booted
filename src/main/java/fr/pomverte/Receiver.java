80816da601267103a52abeac44d34822dcc0b4c4
pomverte <vietnem@yahoo.fr>
2017-07-02 14:34:05 +0200

    typo

---
+++ src/main/java/fr/pomverte/Receiver.java
@@ -0,0 +1,20 @@
+package fr.pomverte;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CountDownLatch;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class Receiver {
+
+    private CountDownLatch latch;
+
+    public void receiveMessage(String message) {
+        log.info("Received '{}'", message);
+        this.latch.countDown();
+    }
+}
